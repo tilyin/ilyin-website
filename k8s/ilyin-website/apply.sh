fa0b4b921e79911071a1a2a0fc8de9ef666ba410
Tim Ilyin <tilyin@gmail.com>
2023-01-09 22:24:06 +0300

    script to apply app reworked at 2023-01-09_T_19:24:06_UTC

--- k8s/ilyin-website/apply.sh
+++ k8s/ilyin-website/apply.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-kubectl delete ns ilyin-website
-wait
+#kubectl delete ns ilyin-website
+#wait
 kubectl apply -f ilyin-website-namespace.yaml
 kubectl apply -f www-ilyin-website-secret.yaml
 kubectl apply -f www-ilyin-website.yaml
